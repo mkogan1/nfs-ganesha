bb3c18a6b6296fe6d2d54f842df9adf5d7600fe8
Daniel Gryniewicz <dang@redhat.com>
2019-06-19 11:12:06 -0400

    MDCACHE - delay invalidate of dir for whence-is-name
    A directory invalidate on a whence-is-name FSAL is a potentially huge
    performance problem.  It can cause the entire directory to reload to
    find the correct cookie, which can time out the client if the directory
    is large.
    
    To avoid this, put of the invalidate as long as active readdirs are in
    process.
    
    Note, this has no effect on upcalls, so an upcall can still invalidate
    the directory.
    
    Resolves: rhbz#1708587
    Resolves: rhbz#1715954
    
    Change-Id: Ice244c5cff6b7b4ba9a83c5e0750e3e27a59b2e5
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -2898,6 +2898,15 @@ fsal_status_t mdcache_readdir_chunked(mdcache_entry_t *directory,
 		has_write = false;
 	}
 
+	if (whence_is_name) {
+		/* While we're getting active readdirs, we don't want to
+		 * invalidate a whence-is-name directory.  This will cause the
+		 * entire directory to be reloaded, causing a huge delay that
+		 * can cause the readdir to time out on the client.  To avoid
+		 * this, bump the expire time on the directory */
+		directory->attrs.expire_time_attr += 15;
+	}
+
 restart:
 	if (look_ck == 0) {
 		/* If starting from beginning, use the first_ck from the
@@ -2992,6 +3001,11 @@ again:
 				 */
 				atomic_set_uint32_t_bits(&directory->mde_flags,
 							 MDCACHE_DIR_POPULATED);
+			}
+
+			if (whence_is_name) {
+				directory->attrs.expire_time_attr =
+					op_ctx->export_perms->expire_time_attr;
 			}
 
 			PTHREAD_RWLOCK_unlock(&directory->content_lock);
@@ -3237,6 +3251,11 @@ again:
 		status = entry->obj_handle.obj_ops->getattrs(&entry->obj_handle,
 							    &attrs);
 		if (FSAL_IS_ERROR(status)) {
+			if (whence_is_name) {
+				directory->attrs.expire_time_attr =
+					op_ctx->export_perms->expire_time_attr;
+			}
+
 			PTHREAD_RWLOCK_unlock(&directory->content_lock);
 
 			LogFullDebugAlt(COMPONENT_NFS_READDIR,
@@ -3317,6 +3336,11 @@ again:
 				 * hang around until the directory is
 				 * invalidated. */
 				mdc_unref_chunk_dirents(chunk, dirent);
+			}
+
+			if (*eod_met && whence_is_name) {
+				directory->attrs.expire_time_attr =
+					op_ctx->export_perms->expire_time_attr;
 			}
 
 			LogDebugAlt(COMPONENT_NFS_READDIR,
