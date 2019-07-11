69c1e0aafc27bee1f1c288a423e85f709ddff0eb
Daniel Gryniewicz <dang@redhat.com>
2019-07-11 11:02:31 -0400

    MDCACHE - Drop chunk ref before dropping lock
    Dropping the content_lock can result in the directory being invalidated,
    causing chunks to hang.  Always drop the chunk ref *before* dropping the
    lock.
    
    Change-Id: I6ee1707733bce5f81127204c3060e45902a29f94
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>
    (cherry picked from commit e01cfff29e00a7c950556ad84d65fd6eb5a1be61)
    (cherry picked from commit 55425593330302981d0b48658e109b7631be0f88)

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -2937,12 +2937,18 @@ again:
 		if (!has_write) {
 			/* Upgrade to write lock and retry just in case
 			 * another thread managed to populate this cookie
-			 * in the meantime.
+			 * in the meantime.  We need to drop our ref on chunk
+			 * (if any) since it could be nuked when we drop the
+			 * lock.
 			 */
+			if (chunk) {
+				mdcache_lru_unref_chunk(chunk);
+				chunk = NULL;
+			}
+
 			PTHREAD_RWLOCK_unlock(&directory->content_lock);
 			PTHREAD_RWLOCK_wrlock(&directory->content_lock);
 			has_write = true;
-			/* We have a ref on chunk; don't null it out */
 			goto again;
 		}
 
@@ -3127,20 +3133,20 @@ again:
 				 */
 				look_ck = dirent->ck;
 
-				PTHREAD_RWLOCK_unlock(&directory->content_lock);
-				PTHREAD_RWLOCK_wrlock(&directory->content_lock);
-				has_write = true;
-
-				/* Dropping the content_lock may have
-				 * invalidated some or all of the dirents and/or
-				 * chunks in this directory.  We need to start
-				 * over from this point.  look_ck is now correct
-				 * if the dirent is still cached, and we haven't
-				 * changed next_ck, so it's still correct for
-				 * reloading the chunk.
+				/* Dropping the content_lock may invalidate some
+				 * or all of the dirents and/or chunks in this
+				 * directory.  We need to start over from this
+				 * point.  look_ck is now correct if the dirent
+				 * is still cached, and we haven't changed
+				 * next_ck, so it's still correct for reloading
+				 * the chunk.
 				 */
 				mdcache_lru_unref_chunk(chunk);
 				chunk = NULL;
+
+				PTHREAD_RWLOCK_unlock(&directory->content_lock);
+				PTHREAD_RWLOCK_wrlock(&directory->content_lock);
+				has_write = true;
 
 				/* Now we need to look for this dirent again.
 				 * We haven't updated next_ck for this dirent
@@ -3179,6 +3185,8 @@ again:
 						     &entry, NULL);
 
 			if (FSAL_IS_ERROR(status)) {
+				mdcache_lru_unref_chunk(chunk);
+
 				PTHREAD_RWLOCK_unlock(&directory->content_lock);
 
 				LogFullDebugAlt(COMPONENT_NFS_READDIR,
@@ -3188,8 +3196,6 @@ again:
 
 				if (status.major == ERR_FSAL_STALE)
 					mdcache_kill_entry(directory);
-
-				mdcache_lru_unref_chunk(chunk);
 				return status;
 			}
 
@@ -3257,6 +3263,7 @@ again:
 					op_ctx->export_perms->expire_time_attr;
 			}
 
+			mdcache_lru_unref_chunk(chunk);
 			PTHREAD_RWLOCK_unlock(&directory->content_lock);
 
 			LogFullDebugAlt(COMPONENT_NFS_READDIR,
@@ -3265,7 +3272,6 @@ again:
 					fsal_err_txt(status));
 
 			mdcache_put(entry);
-			mdcache_lru_unref_chunk(chunk);
 			return status;
 		}
 
