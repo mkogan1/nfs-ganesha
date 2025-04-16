25c8e6caa54eac8c000a73d8ba3a19efc7587650
Marcus Watts <mwatts@redhat.com>
2025-04-16 14:01:10 -0400

    fscrypt kmip module - get key from kmip, give to ceph fsal module.
    This works as a export extension plugin and adds extra global and
    per-export configuration options to configure parameters to talk to the
    kmip host, and to associate key information with the corresponding export.
    
    If a key cannot be fetched, the correpsonding export is not
    made available.
    
    This uses "libkmip" and "OpenSSL" to talk to the kmip server.
    
    Client intermediate certificate are supported, as well as SNI.
    
    Change-Id: I21ddced4ab0dbb083ee40eb11e485443e11ebe21
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    (cherry picked from commit 2d234bdb132bf15fcf62cde83021db5a0003b748)

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -138,6 +138,7 @@ BuildRequires:	flex
 BuildRequires:	pkgconfig
 BuildRequires:	userspace-rcu-devel
 BuildRequires:	krb5-devel
+BuildRequires:	openssl-devel
 %if %{with rados_recov} || %{with rados_urls}
 BuildRequires: librados-devel >= 0.61
 %endif
@@ -751,6 +752,7 @@ exit 0
 %files
 %{_bindir}/ganesha.nfsd
 %{_libdir}/libganesha_nfsd.so*
+%{_libdir}/ganesha/libkmip_fscrypt*
 %config %{_sysconfdir}/dbus-1/system.d/org.ganesha.nfsd.conf
 %config(noreplace) %{_sysconfdir}/sysconfig/ganesha
 %config(noreplace) %{_sysconfdir}/logrotate.d/ganesha
