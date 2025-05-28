78a80fc78e5a3e1a46d78dd4d7cd80e55f51d0e0
Sachin Punadikar <sachin.punadikar@ibm.com>
2025-05-28 11:04:45 -0400

    CEPH: Support old logic of ceph uuid generation
    Added code for supporting old logic of ceph client uuid generation. This
    logic will help old NFS Ganesha setups to continue work without
    disruption during upgrade.
    
    Change-Id: I3a29c1a3ecc2a0319d172419ea6cfd63892232a6
    Signed-off-by: Sachin Punadikar <sachin.punadikar@ibm.com>

--- src/FSAL/FSAL_CEPH/main.c
+++ src/FSAL/FSAL_CEPH/main.c
@@ -126,6 +126,7 @@ static struct config_item ceph_items[] = {
 	CONF_ITEM_BOOL("client_oc", false, ceph_fsal_module, client_oc),
 	CONF_ITEM_BOOL("async", false, ceph_fsal_module, async),
 	CONF_ITEM_BOOL("zerocopy", false, ceph_fsal_module, zerocopy),
+	CONF_ITEM_BOOL("use_old_uuid", false, ceph_fsal_module, use_old_uuid),
 	CONFIG_EOL
 };
 
@@ -357,19 +358,30 @@ static inline void enable_delegations(struct ceph_mount *cm)
 void create_unique_id(struct ceph_mount *cm, char *nodeid, char **uniq_id)
 {
 	size_t len;
-	char buff[8192]; /* large buffer to accommodate lengthy path */
-	uint64_t hashkey;
 
-	/* create string containing nodeid, userid, fs_name and mount path */
-	(void)snprintf(buff, 8192, "%s%s%s%s", nodeid, cm->cm_user_id,
-		       cm->cm_fs_name, cm->cm_mount_path);
-	LogEvent(COMPONENT_FSAL, "ceph_mount hash data: %s", buff);
-	hashkey = CityHash64(buff, strlen(buff));
-	len = strlen(RECLAIM_UUID_PREFIX) + 128 + 1;
-	*uniq_id = gsh_malloc(len);
-	/* uniq_id will be always like "ganesha-<64-bytes-hash>" */
-	(void)snprintf(*uniq_id, len, RECLAIM_UUID_PREFIX "0x%" PRIx64,
-		       hashkey);
+	if (CephFSM.use_old_uuid) {
+		LogDebug(COMPONENT_FSAL, "Old logic of uuid for ceph");
+		len = strlen(RECLAIM_UUID_PREFIX) + strlen(nodeid) + 1 + 4 + 1;
+		*uniq_id = gsh_malloc(len);
+		(void)snprintf(*uniq_id, len, RECLAIM_UUID_PREFIX "%s-%4.4hx",
+			       nodeid, cm->cm_export_id);
+	} else {
+		char buff[8192]; /* large buffer to accommodate lengthy path */
+		uint64_t hashkey;
+
+		LogDebug(COMPONENT_FSAL, "New logic of uuid for ceph");
+		/* create string containing nodeid, userid, fs_name and mount
+		 * path for hashing purpose*/
+		(void)snprintf(buff, 8192, "%s%s%s%s", nodeid, cm->cm_user_id,
+			       cm->cm_fs_name, cm->cm_mount_path);
+		LogDebug(COMPONENT_FSAL, "ceph_mount hash data: %s", buff);
+		hashkey = CityHash64(buff, strlen(buff));
+		len = strlen(RECLAIM_UUID_PREFIX) + 128 + 1;
+		*uniq_id = gsh_malloc(len);
+		/* uniq_id will be always like "ganesha-<64-bytes-hash>" */
+		(void)snprintf(*uniq_id, len, RECLAIM_UUID_PREFIX "0x%" PRIx64,
+			       hashkey);
+	}
 	LogDebug(COMPONENT_FSAL, "Unique id for ceph_mount : %s", *uniq_id);
 }
 
