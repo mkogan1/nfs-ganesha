31b6ab1a4c2d7799b16fcd96969366c08a4ca4ab
Sachin Punadikar <sachin.punadikar@ibm.com>
2025-10-08 10:08:09 -0400

    Support disabling dentry caching for export
    By default, mdcache caches dentries unless disabled by config option
    "Dir_Chunk = 0". Caching of dentries helps in improved performance.
    In certain scenarios like underneath FSAL recommends not to cache
    dentries (like CEPH) or in case the export is getting changed outside
    of NFS protocol, then disabling dentry caching helps.
    One need to add below config value in EXPORT block.
       DisableDirCaching = true;
    
    Change-Id: I5140c19f6fae3861d56413afe43d78517a55bc4b
    Signed-off-by: Sachin Punadikar <sachin.punadikar@ibm.com>

--- src/support/exports.c
+++ src/support/exports.c
@@ -2359,6 +2359,9 @@ static struct config_item fsal_params[] = {
 		CONF_ITEM_BOOLBIT_SET("DisableReaddirPlus", false,             \
 				      EXPORT_OPTION_NO_READDIR_PLUS, _struct_, \
 				      options, options_set),                   \
+		CONF_ITEM_BOOLBIT_SET("DisableDirCaching", false,              \
+				      EXPORT_OPTION_NO_DIR_CACHING, _struct_,  \
+				      options, options_set),                   \
 		CONF_ITEM_BOOLBIT_SET(                                         \
 			"Trust_Readdir_Negative_Cache", false,                 \
 			EXPORT_OPTION_TRUST_READIR_NEGATIVE_CACHE, _struct_,   \
@@ -2515,6 +2518,9 @@ static void *pseudofs_init(void *link_mem, void *self_struct)
 				      _struct_, options, options_set),         \
 		CONF_ITEM_BOOLBIT_SET("DisableReaddirPlus", false,             \
 				      EXPORT_OPTION_NO_READDIR_PLUS, _struct_, \
+				      options, options_set),                   \
+		CONF_ITEM_BOOLBIT_SET("DisableDirCaching", false,              \
+				      EXPORT_OPTION_NO_DIR_CACHING, _struct_,  \
 				      options, options_set),                   \
 		CONF_ITEM_BOOLBIT_SET(                                         \
 			"Trust_Readdir_Negative_Cache", false,                 \
